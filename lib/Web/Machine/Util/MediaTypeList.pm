203ce277cfd4329fd991a9254478760df14ab297
Stevan Little <stevan.little@iinteractive.com>
2012-02-29 07:40:01 -0500

    do proper sorting of specificity

--- lib/Web/Machine/Util/MediaTypeList.pm
+++ lib/Web/Machine/Util/MediaTypeList.pm
@@ -8,8 +8,42 @@ extends 'Web::Machine::Util::PriorityList';
 sub add_header_value {
     my ($self, $c) = @_;
     my $mt = Web::Machine::Util::MediaType->new_from_string( $c );
+    # NOTE:
+    # we delete the q param here because we
+    # do not want it to be involved in the
+    # matching of the params.
+    # - SL
     my $q  = delete $mt->params->{ 'q' } || 1.0;
     $self->add( $q, $mt );
+}
+
+sub iterable {
+    my $self = shift;
+    # From RFC-2616 sec14
+    # Media ranges can be overridden by more specific
+    # media ranges or specific media types. If more
+    # than one media range applies to a given type,
+    # the most specific reference has precedence.
+    sort {
+        if ( $a->[0] == $b->[0] ) {
+            $a->[1]->matches_all
+                ? 1
+                : ($b->[1]->matches_all
+                    ? -1
+                    : ($a->[1]->minor eq '*'
+                        ? 1
+                        : ($b->[1]->minor eq '*'
+                            ? -1
+                            : ($a->[1]->params_are_empty
+                                ? 1
+                                : ($b->[1]->params_are_empty
+                                    ? - 1
+                                    : 0)))))
+        }
+        else {
+            $b->[0] <=> $a->[0]
+        }
+    } $self->SUPER::iterable;
 }
 
 __PACKAGE__->meta->make_immutable;
