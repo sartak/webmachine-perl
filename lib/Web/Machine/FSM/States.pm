6200c240da383e174b00915cc7ea48b2431524ff
Stevan Little <stevan.little@iinteractive.com>
2012-03-02 13:28:51 -0500

    check point commit, going well here

--- lib/Web/Machine/FSM/States.pm
+++ lib/Web/Machine/FSM/States.pm
@@ -208,43 +208,71 @@ sub f6 {
     (not $request->header('Accept-Encoding')) ? \&g7 : \&f7;
 }
 
+# Acceptable encoding available?
 sub f7 {
     my ($resource, $request, $response, $metadata) = @_;
-    # FIXME: this needs work and is most likely totally wrong.
-    _choose( $request->header('Accept-Encoding'), $resource->encodings_provided ) ? \&g7 : \406;
+
+    if ( my $encoding = choose_encoding( $request->header('Accept-Encoding'), $resource->encodings_provided ) ) {
+        $response->header( 'Content-Encoding' => $encoding ) unless $encoding eq 'identity';
+        $metadata->{'Content-Encoding'} = $encoding;
+        return \&g7;
+    }
+
+    return \406;
 }
 
+# Resource exists?
 sub g7 {
     my ($resource, $request, $response, $metadata) = @_;
-    # TODO: Need to add the Vary header setting stuff here ...
+
+    # NOTE:
+    # set Vary header here since we are
+    # done with content negotiation
+    # - SL
+    my @variances = @{ $resource->variances };
+
+    push @variances => 'Accept'          if @{ $resource->content_types_provided };
+    push @variances => 'Accept-Language' if @{ $resource->languages_provided };
+    push @variances => 'Accept-Charset'  if @{ $resource->charsets_provided };
+    push @variances => 'Accept-Encoding' if keys %{ $resource->encodings_provided };
+
+    $response->header( 'Vary' => join ', ' => @variances ) if @variances;
+
     $resource->resource_exists == true ? \&g8 : \&h7;
 }
 
+# If-Match exists?
 sub g8 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->header('If-Match') ? \&g9 : \&h10;
 }
 
+# If-Match: * exists?
 sub g9 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->header('If-Match') eq "*" ? \&h10 : \&g11;
 }
 
+# ETag in If-Match
 sub g11 {
     my ($resource, $request, $response, $metadata) = @_;
-    _choose( $resource->generate_etag, [ split /\s*\,\s*/ => $request->header('If-Match') ] ) ? \&h10 : \412;
+    my @etags = map { unquote_header( $_ ) } split /\s*\,\s*/ => $request->header('If-Match');
+    _include( $resource->generate_etag, \@etags ) ? \&h10 : \412;
 }
 
+# If-Match exists?
 sub h7 {
     my ($resource, $request, $response, $metadata) = @_;
-    ($request->header('If-Match') && $request->header('If-Match') == "*") ? \412 : \&i7;
+    ($request->header('If-Match') && unquote_header( $request->header('If-Match') ) eq "*") ? \412 : \&i7;
 }
 
+# If-Unmodified-Since exists?
 sub h10 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->header('If-Unmodified-Since') ? \&h11 : \&i12;
 }
 
+# If-Unmodified-Since is valid date?
 sub h11 {
     my ($resource, $request, $response, $metadata) = @_;
     if ( my $date = str2time( $request->header('If-Unmodified-Since') ) ) {
@@ -254,78 +282,104 @@ sub h11 {
     return \&i12;
 }
 
+# Last-Modified > I-UM-S?
 sub h12 {
     my ($resource, $request, $response, $metadata) = @_;
-    (defined $resource->last_modified && str2time( $resource->last_modified ) > $metadata->{'If-Unmodified-Since'}) ? \412 : \&i12;
+    defined $resource->last_modified
+        &&
+    (str2time( $resource->last_modified ) > $metadata->{'If-Unmodified-Since'})
+        ? \412 : \&i12;
 }
 
+# Moved permanently? (apply PUT to different URI)
 sub i4 {
     my ($resource, $request, $response, $metadata) = @_;
     if ( my $uri = $resource->moved_permanently ) {
+        if ( is_status_code( $uri ) ) {
+            return $uri;
+        }
         $response->header('Location' => $uri );
         return \301;
     }
     return \&p3;
 }
 
+# PUT?
 sub i7 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->method eq 'PUT' ? \&i4 : \&k7
 }
 
+# If-none-match exists?
 sub i12 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->header('If-None-Match') ? \&i13 : \&l13
 }
 
+# If-none-match: * exists?
 sub i13 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->header('If-None-Match') eq "*" ? \&j13 : \&k13
 }
 
+# GET or HEAD?
 sub j18 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->method eq 'GET' || $request->method eq 'HEAD' ? \304 : \412
 }
 
+# Moved permanently?
 sub k5 {
     my ($resource, $request, $response, $metadata) = @_;
     if ( my $uri = $resource->moved_permanently ) {
+        if ( is_status_code( $uri ) ) {
+            return $uri;
+        }
         $response->header('Location' => $uri );
         return \301;
     }
     return \&l5;
 }
 
+# Previously existed?
 sub k7 {
     my ($resource, $request, $response, $metadata) = @_;
     $resource->previously_existed == true ? \&k5 : \&l7;
 }
 
+# Etag in if-none-match?
 sub k13 {
     my ($resource, $request, $response, $metadata) = @_;
-    _choose( $resource->generate_etag, [ split /\s*\,\s*/ => $request->header('If-Match') ] ) ? \&j18 : \&l13;
+    my @etags = map { unquote_header( $_ ) } split /\s*\,\s*/ => $request->header('If-None-Match');
+    _include( $resource->generate_etag, \@etags ) ? \&j18 : \&l13;
 }
 
+# Moved temporarily?
 sub l5 {
     my ($resource, $request, $response, $metadata) = @_;
     if ( my $uri = $resource->moved_temporarily ) {
+        if ( is_status_code( $uri ) ) {
+            return $uri;
+        }
         $response->header('Location' => $uri );
         return \307;
     }
     return \&m5;
 }
 
+# POST?
 sub l7 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->method eq 'POST' ? \&m7 : \404
 }
 
+# If-Modified-Since exists?
 sub l13 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->header('If-Modified-Since') ? \&l14 : \&m16
 }
 
+# If-Modified-Since is valid date?
 sub l14 {
     my ($resource, $request, $response, $metadata) = @_;
     if ( my $date = str2time( $request->header('If-Modified-Since') ) ) {
@@ -335,58 +389,74 @@ sub l14 {
     return \&m16;
 }
 
+# If-Modified-Since > Now?
 sub l15 {
     my ($resource, $request, $response, $metadata) = @_;
     ($metadata->{'If-Modified-Since'} > (scalar time)) ? \&m16 : \&l17;
 }
 
+# Last-Modified > If-Modified-Since?
 sub l17 {
     my ($resource, $request, $response, $metadata) = @_;
-    (defined $resource->last_modified && str2time( $resource->last_modified ) > $metadata->{'If-Modified-Since'}) ? \&m16 : \304;
+    defined $resource->last_modified
+        &&
+    (str2time( $resource->last_modified ) > $metadata->{'If-Modified-Since'})
+        ? \&m16 : \304;
 }
 
+# POST?
 sub m5 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->method eq 'POST' ? \&n5 : \410
 }
 
+# Server allows POST to missing resource?
 sub m7 {
     my ($resource, $request, $response, $metadata) = @_;
     $resource->allow_missing_post == true ? \&n11 : \404
 }
 
+# DELETE?
 sub m16 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->method eq 'DELETE' ? \&m20 : \&n16
 }
 
+# DELETE enacted immediately? (Also where DELETE is forced.)
 sub m20 {
     my ($resource, $request, $response, $metadata) = @_;
     $resource->delete_resource == true ? \&m20b : \500
 }
 
+# Did the DELETE complete?
 sub m20b {
     my ($resource, $request, $response, $metadata) = @_;
     $resource->delete_completed == true ? \&o20 : \202
 }
 
+# Server allows POST to missing resource?
 sub n5 {
     my ($resource, $request, $response, $metadata) = @_;
     $resource->allow_missing_post == true ? \&n11 : \410
 }
 
+# Redirect?
 sub n11 {
-    die "n11"
+    my ($resource, $request, $response, $metadata) = @_;
+    # ...
 }
 
+# POST?
 sub n16 {
     my ($resource, $request, $response, $metadata) = @_;
     $request->method eq 'POST' ? \&n11 : \&o16
 }
 
+# Conflict?
 sub o14 {
     my ($resource, $request, $response, $metadata) = @_;
     return \409 if $resource->is_conflict;
+    #...
 
 }
 
