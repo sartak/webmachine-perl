c05a296f90ae4c0717e40abd5b28d7f5725dfa9c
Stevan Little <stevan.little@iinteractive.com>
2012-03-01 22:09:50 -0500

    charset negotiation

--- lib/Web/Machine/Util/ContentNegotiation.pm
+++ lib/Web/Machine/Util/ContentNegotiation.pm
@@ -13,6 +13,7 @@ use Sub::Exporter -setup => {
     exports => [qw[
         choose_media_type
         choose_language
+        choose_charset
     ]]
 };
 
@@ -69,45 +70,26 @@ sub choose_language {
     $language;
 }
 
-# sub choose_encoding {
-#     my ($provided, $header) = @_;
-#     do_choose( [ keys %$provided ], $header, 'identity' );
-# }
-#
-# sub choose_charset {
-#     my ($provided, $header) = @_;
-#     if ( @$provided ) {
-#         my $charsets = map {  } @$provided;
-#     }
-# }
-#
-# sub do_choose {
-#     my ($choices, $header, $default) = @_;
-#
-#     $choices = map { lc $_ } $choices;
-#
-#     my $accepted         = Web::Machine::Util::PriorityList->new_from_header_list( split /\s*, \s/ => $header );
-#     my $default_priority = $accepted->priority_of( $default );
-#     my $star_priority    = $accepted->priority_of( '*' );
-#     my $default_ok       = ( defined $default_priority && $star_priority != 0.0 ) || $default_priority;
-#     my $any_ok           = $star_priority && $star_priority > 0.0;
-#
-#     my $chosen = find {
-#         my ($priority, $acceptable) = @$_;
-#         if ( $priority == 0.0 ) {
-#             $choices = grep { lc $acceptable ne $_ } @$choices;
-#             false;
-#         } else {
-#             first { lc $acceptable eq $_ } @$choices
-#         }
-#     } $accepted->iterable;
-#
-#     ($chosen)
-#         ||
-#     ($any_ok && $choices->[0])
-#         ||
-#     ($default_ok && (first { $default eq $_ } @$choices) && $default)
-# }
+sub choose_charset {
+    my ($provided, $header) = @_;
+    if ( @$provided ) {
+        my @charsets = map { $_->[0] } @$provided;
+        # NOTE:
+        # Making the default charset UTF-8, which
+        # is maybe sensible, I dunno.
+        # - SL
+        if ( my $charset = make_choice( \@charsets, $header, 'UTF-8' )) {
+            return $charset;
+        }
+    }
+    return;
+}
+
+sub choose_encoding {
+    my ($provided, $header) = @_;
+}
+
+## ....
 
 sub media_match {
     my ($requested, $provided) = @_;
@@ -119,6 +101,39 @@ sub language_match {
     my ($range, $tag) = @_;
     ((lc $range) eq (lc $tag)) || $range eq "*" || $tag =~ /^$range\-/i;
 }
+
+sub make_choice {
+    my ($choices, $header, $default) = @_;
+
+    $choices = [ map { lc $_ } @$choices ];
+
+    my $accepted         = Web::Machine::Util::PriorityList->new_from_header_list( split /\s*,\s*/ => $header );
+    my $default_priority = $accepted->priority_of( $default );
+    my $star_priority    = $accepted->priority_of( '*' ) || 0.0;
+    my $default_ok       = ( defined $default_priority && $star_priority != 0 ) || $default_priority;
+    my $any_ok           = $star_priority && $star_priority > 0.0;
+
+    #use Data::Dumper; warn Dumper $accepted;
+
+    my $chosen = first {
+        my ($priority, $acceptable) = @$_;
+        if ( $priority == 0.0 ) {
+            $choices = [ grep { lc $acceptable ne $_ } @$choices ];
+            return 0;
+        } else {
+            return any { lc $acceptable eq $_ } @$choices;
+        }
+    } $accepted->iterable;
+
+    #use Data::Dumper; warn Dumper $chosen;
+
+    ($chosen && $chosen->[-1])
+        ||
+    ($any_ok && $choices->[0])
+        ||
+    ($default_ok && (any { $default eq $_ } @$choices) && $default)
+}
+
 
 1;
 
---
+++ t/302-content-negotiation-charset.t
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Fatal;
+
+BEGIN {
+    use_ok('Web::Machine::Util::ContentNegotiation', 'choose_charset');
+}
+
+ok(!defined( choose_charset( [], 'ISO-8859-1' ) ), '... got nothing back');
+
+is(
+    choose_charset( [[ "UTF-8", sub {} ],[ "US-ASCII", sub {} ]], "US-ASCII, UTF-8" ),
+    'US-ASCII',
+    '... got the right charset back'
+);
+
+is(
+    choose_charset( [[ "UTF-8", sub {} ],[ "US-ASCII", sub {} ]], "US-ASCII;q=0.7, UTF-8" ),
+    'UTF-8',
+    '... got the right charset back'
+);
+
+ok(!defined( choose_charset( [[ "UTF-8", sub {} ],[ "US-ASCII", sub {} ]], 'ISO-8859-1' ) ), '... got nothing back');
+
+is(
+    choose_charset( [[ "UtF-8", sub {} ],[ "US-ASCII", sub {} ]], "iso-8859-1, utf-8" ),
+    'utf-8',
+    '... got the right charset back'
+);
+
+done_testing;
