ba7960e5c28e88fb5c60b57425b0be86b76f0589
Stevan Little <stevan.little@iinteractive.com>
2012-03-05 13:02:56 -0500

    charsets_provided should return undef by default so that the short-circuiting happens correctly

--- lib/Web/Machine/Resource.pm
+++ lib/Web/Machine/Resource.pm
@@ -34,7 +34,7 @@ sub base_uri                  { undef }
 sub process_post              { 0 }
 sub content_types_provided    { [] }
 sub content_types_accepted    { [] }
-sub charsets_provided         { [] }
+sub charsets_provided         { undef }
 sub languages_provided        { [] }
 sub encodings_provided        { { 'identity' => sub { $_[1] } } }
 sub variances                 { [] }
