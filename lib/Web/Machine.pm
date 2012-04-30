4fb8626a8e8aeb1e6575d67cd940074d614f294f
Stevan Little <stevan.little@iinteractive.com>
2012-04-30 08:53:44 -0400

    breaking down the methods in call and documenting them

--- lib/Web/Machine.pm
+++ lib/Web/Machine.pm
@@ -29,25 +29,36 @@ sub new {
 
 sub inflate_request {
     my ($self, $env) = @_;
+    HTTP::Headers::ActionPack->new->inflate( Plack::Request->new( $env ) );
+}
 
-    my $request = Plack::Request->new( $env );
+sub create_fsm {
+    my $self = shift;
+    Web::Machine::FSM->new( tracing => $self->{'tracing'} )
+}
 
-    HTTP::Headers::ActionPack->new->inflate( $request );
+sub create_resource {
+    my ($self, $request) = @_;
+    $self->{'resource'}->new(
+        request  => $request,
+        response => $request->new_response
+    );
+}
 
-    $request;
+sub finalize_response {
+    my ($self, $response) = @_;
+    $response->finalize;
 }
 
 sub call {
     my ($self, $env) = @_;
 
-    my $request = $self->inflate_request( $env );
+    my $request  = $self->inflate_request( $env );
+    my $resource = $self->create_resource( $request );
+    my $fsm      = $self->create_fsm;
 
-    return Web::Machine::FSM->new( %{ $self->{'fsm_args'} || {} } )->run(
-        $self->{'resource'}->new(
-            request  => $request,
-            response => $request->new_response,
-        )
-    )->finalize;
+    my $response = $fsm->run( $resource );
+    $self->finalize_response( $response );
 }
 
 1;
@@ -102,6 +113,47 @@ This module is extremely young and it is a port of an pretty young (June 2011)
 module in another language (ruby), which itself is a port of a still kind of
 young module (March 2009) in yet another language (erlang). But that all said,
 it really seems like a sane idea and so I stole it and ported it to Perl.
+
+=head1 METHODS
+
+NOTE: This module is a L<Plack::Component> subclass and so follows the interface
+set forward by that module.
+
+=over 4
+
+=item C<new( resource => $resource_classname, ?tracing => 1|0 )>
+
+The constructor expects to get a C<$resource_classname> and can take an optional
+C<tracing> parameter which it will pass onto the L<Web::Machine::FSM>.
+
+=item C<inflate_request( $env )>
+
+This takes a raw PSGI C<$env> and inflates it into a L<Plack::Request> instance.
+By default this also uses L<HTTP::Headers::ActionPack> to inflate the headers
+of the request to be complex objects.
+
+=item C<create_fsm>
+
+This will create the L<Web::Machine::FSM> object to run. It will get passed
+the value of the C<tracing> constructor parameter.
+
+=item C<create_resource( $request )>
+
+This will create the L<Web::Machine::Resource> instance using the class specified
+in the C<resource> constructor parameter. It will pass in the C<$request> object
+and call C<new_response> on the C<$request> object to get a L<Plack::Response>
+instance.
+
+=item C<finalize_response( $response )>
+
+Given a C<$response> which is a L<Plack::Response> object, this will finalize
+it and return a raw PSGI response.
+
+=item C<call( $env )>
+
+This is the C<call> method overridden from the L<Plack::Component> superclass.
+
+=back
 
 =head1 DEBUGGING
 
