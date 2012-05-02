d1e0a23d0c3f35e12964f45cc5ecb7c4aac4973a
Stevan Little <stevan.little@iinteractive.com>
2012-05-02 10:56:57 -0400

    more docs

--- lib/Web/Machine/Util.pm
+++ lib/Web/Machine/Util.pm
@@ -92,5 +92,53 @@ These two functions are used for fetching the key
 and value out of a pair in the L<Web::Machine> internals.
 We represent a pair simply as a HASH ref with one key.
 
+=item C<create_date( $date_string | $time_peice )>
+
+Given either a C<$date_string> or an instance of L<Time::Piece>,
+this will inflate it into a L<HTTP::Headers::ActionPack::DateHeader>
+object, suitable for use in the FSM.
+
+=item C<bind_path( $path_spec, $path )>
+
+Given a C<$path_spec> (described below) and a C<$path>, this will
+either bind the path to the spec and return and array of bound
+values, or it will return nothing. Returning nothing indicates
+that no match was found. Additionally, if this function is called
+in scalar context, and there is only one match, it will return
+that item. Otherwise it will return the array as normal. This all
+makes it easy to use the following idiom:
+
+  if ( my $id = bind_path( '/:id', $request->path_info ) ) {
+      # handle the case with an ID here
+  }
+  else {
+      # handle other cases here
+  }
+
+The C<$path_spec> follows a pretty standard convention. Literal
+path parts must match corresponding literal. Variable path parts
+are prefixed by a colon and are captured for returning later.
+And lastly the "splat" operator (C<*>) is supported and causes
+all the rest of the path segements to be returned. Below are a
+few examples of this:
+
+  spec                  path             result
+  ------------------------------------------------------------
+  /test/:foo/:bar       /test/1/2        ( 1, 2 )
+  /test/*               /test/1/2/3      ( 1, 2, 3 )
+  /user/:id/:action     /user/1/edit     ( 1, 'edit' )
+  /:id                  /201             ( 201 )
+
+This function is kept deliberately simple and it is expected
+that the user will use C<my> in the array form to assign
+multiple variables, like this:
+
+  my ( $foo, $bar ) = bind_path( '/test/:foo/:bar', $path );
+
+In the future we might add a C<bind_path_hash> function which
+captures the variable names as well, but to be honest, if you
+feel you need that, you likely want one of the many excellent
+path dispatching modules available on CPAN.
+
 =back
 
