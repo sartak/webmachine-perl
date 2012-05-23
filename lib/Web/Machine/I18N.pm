a63b9b5cd247e0ff2335c0710f9ae9dc5946637a
Stevan Little <stevan.little@iinteractive.com>
2012-05-23 14:50:19 -0400

    basic infrastructure for some i18n handling

---
+++ lib/Web/Machine/I18N.pm
@@ -0,0 +1,24 @@
+package Web::Machine::I18N;
+# ABSTRACT: The I18N support for HTTP information
+
+use strict;
+use warnings;
+
+use parent 'Locale::Maketext';
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  use Web::Machine::I18N;
+
+=head1 DESCRIPTION
+
+
+
+
+
+
+
