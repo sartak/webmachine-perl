ef44982002ff26a8d3d249e324ea7fd8d03eca26
Dave Rolsky <autarch@urth.org>
2013-04-15 17:18:47 -0500

    Require HTTP::Headers::ActionPack 0.05

--- lib/Web/Machine/Util.pm
+++ lib/Web/Machine/Util.pm
@@ -8,7 +8,7 @@ use Carp         qw[ confess ];
 use Scalar::Util qw[ blessed ];
 use List::Util   qw[ first ];
 
-use HTTP::Headers::ActionPack 0.04;
+use HTTP::Headers::ActionPack 0.05;
 
 use Sub::Exporter -setup => {
     exports => [qw[
