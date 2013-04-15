3e8c66edb0e613bdad61cc2c51f9a04210179714
Dave Rolsky <autarch@urth.org>
2013-04-15 16:34:13 -0500

    Fix testing of charsets_provided
    If the resource provides "utf-8" that will now match the ActionPack default of
    "UTF-8". This didn't used to match because of ActionPack bugs. Now our test
    resource provides "ISO-8859-2" which will never match the default, nor the
    headers in our tests.

--- t/010-resources/E6.pm
+++ t/010-resources/E6.pm
@@ -6,6 +6,6 @@ use parent 'Web::Machine::Resource';
 
 sub content_types_provided { [ { 'text/plain' => sub {} } ] }
 sub languages_provided     { [qw[ de fr ]] }
-sub charsets_provided      { [ { 'utf-8' => sub {} } ] }
+sub charsets_provided      { [ { 'ISO-8859-2' => sub {} } ] }
 
 1;
